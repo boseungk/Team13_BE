de3cb269e23ae5c0102b1898564b8866401d2a0d
46112 <duf325@naver.com>
2023-10-25 21:50:18 +0900

    refactor: 응답값 커서 삭제

--- src/main/java/com/theocean/fundering/domain/comment/dto/CommentResponse.java
+++ src/main/java/com/theocean/fundering/domain/comment/dto/CommentResponse.java
@@ -10,12 +10,12 @@ public class CommentResponse {
   @Getter
   public static class findAllDTO {
     private final List<commentDTO> comments;
-    private final Long lastCursor;
+    private final int currentPage;
     private final boolean isLastPage;
 
-    public findAllDTO(List<commentDTO> comments, Long lastCursor, boolean isLastPage) {
+    public findAllDTO(final List<commentDTO> comments, final int currentPage, final boolean isLastPage) {
       this.comments = comments;
-      this.lastCursor = lastCursor;
+      this.currentPage = currentPage;
       this.isLastPage = isLastPage;
     }
 
@@ -35,17 +35,17 @@ public class CommentResponse {
     private final int depth;
     private final long createdAt;
 
-    public commentDTO(Comment comment, String writerName, String writerProfile) {
-      this.commentId = comment.getCommentId();
-      this.writerId = comment.getWriterId();
+    commentDTO(final Comment comment, final String writerName, final String writerProfile) {
+      commentId = comment.getCommentId();
+      writerId = comment.getWriterId();
       this.writerName = writerName;
       this.writerProfile = writerProfile;
-      this.content = comment.getContent();
-      this.depth = comment.getDepth();
-      this.createdAt = comment.getEpochSecond();
+      content = comment.getContent();
+      depth = comment.getDepth();
+      createdAt = comment.getEpochSecond();
     }
 
-    public static commentDTO fromEntity(Comment comment, String nickname, String profileImage) {
+    public static commentDTO fromEntity(final Comment comment, final String nickname, final String profileImage) {
       return new commentDTO(comment, nickname, profileImage);
     }
   }
