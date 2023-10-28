1cd3f9e920159eaee53fb74f0333291aa2a0883a
46112 <duf325@naver.com>
2023-10-29 00:10:46 +0900

    feat: 댓글 조회 DTO내부에 replyCount 필드 추가

--- src/main/java/com/theocean/fundering/domain/comment/dto/CommentResponse.java
+++ src/main/java/com/theocean/fundering/domain/comment/dto/CommentResponse.java
@@ -10,13 +10,13 @@ public class CommentResponse {
 
     // 전체 댓글 조회 DTO
     @Getter
-    public static class findAllDTO {
-        private final List<commentDTO> comments;
+    public static class FindAllDTO {
+        private final List<CommentDTO> comments;
         private final int currentPage;
         private final boolean isLastPage;
 
-        public findAllDTO(
-                final List<commentDTO> comments, final int currentPage, final boolean isLastPage) {
+        public FindAllDTO(
+                final List<CommentDTO> comments, final int currentPage, final boolean isLastPage) {
             this.comments = comments;
             this.currentPage = currentPage;
             this.isLastPage = isLastPage;
@@ -29,28 +29,29 @@ public class CommentResponse {
 
     // findAllDTO의 내부에 들어갈 댓글 정보 DTO
     @Getter
-    public static class commentDTO {
+    public static class CommentDTO {
         private final Long commentId;
         private final Long writerId;
         private final String writerName;
         private final String writerProfile;
         private final String content;
+        private final int replyCount;
         private final int depth;
         private final long createdAt;
 
-        commentDTO(final Comment comment, final String writerName, final String writerProfile) {
+        CommentDTO(final Comment comment, final int replyCount, final String writerName, final String writerProfile) {
             commentId = comment.getCommentId();
             writerId = comment.getWriterId();
             this.writerName = writerName;
             this.writerProfile = writerProfile;
             content = comment.getContent();
+            this.replyCount = replyCount;
             depth = comment.getDepth();
             createdAt = comment.getEpochSecond();
         }
 
-        public static commentDTO fromEntity(
-                final Comment comment, final String nickname, final String profileImage) {
-            return new commentDTO(comment, nickname, profileImage);
+        public static CommentDTO fromEntity(final Comment comment, final int replyCount, final String nickname, final String profileImage) {
+            return new CommentDTO(comment, replyCount, nickname, profileImage);
         }
     }
 }
