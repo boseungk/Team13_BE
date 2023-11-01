551d13fd88eeab303917c7d23e63ffbde3463ab6
unknown <182876@jnu.ac.kr>
2023-11-02 02:31:49 +0900

    feat: 펀딩 결제 메소드 추가

--- src/main/java/com/theocean/fundering/domain/payment/domain/controller/PaymentController.java
+++ src/main/java/com/theocean/fundering/domain/payment/domain/controller/PaymentController.java
@@ -4,12 +4,16 @@ package com.theocean.fundering.domain.payment.domain.controller;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
+import com.theocean.fundering.domain.member.domain.Member;
 import com.theocean.fundering.domain.payment.domain.dto.PaymentRequest;
 import com.theocean.fundering.domain.payment.domain.service.PaymentService;
+import com.theocean.fundering.global.jwt.JwtProvider;
+import com.theocean.fundering.global.jwt.userInfo.CustomUserDetails;
 import com.theocean.fundering.global.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,18 +26,23 @@ import java.io.IOException;
 @RestController
 public class PaymentController {
     private final PaymentService paymentService;
-
+    private final JwtProvider jwtProvider;
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("/posts/{postId}/verify")
-    public IamportResponse<Payment> verifyByImpUid(@PathVariable Long postId, @RequestParam(value = "imp_uid") String impUid) throws IamportResponseException, IOException {
+    public IamportResponse<Payment> verifyByImpUid(@PathVariable Long postId,
+                                                   @RequestParam(value = "imp_uid") String impUid) throws IamportResponseException, IOException {
         return paymentService.verifyByImpUid(impUid);
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("/posts/{postId}/donate")
-    public ResponseEntity<?> donate(@PathVariable Long postId, @RequestBody PaymentRequest.DonateDTO donateDTO){
-        paymentService.donate(postId, donateDTO);
+    public ResponseEntity<?> donate(@PathVariable Long postId,
+                                    @AuthenticationPrincipal CustomUserDetails userDetails,
+                                    @RequestParam(value = "imp_uid") String impUid,
+                                    @RequestBody PaymentRequest.DonateDTO donateDTO){
+        Member member = userDetails.getMember();
+        paymentService.donate(postId, member, impUid, donateDTO);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 
