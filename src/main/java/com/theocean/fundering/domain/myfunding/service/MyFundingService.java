6ddad0f5949b640d121ab22938af93be63162329
BoSeung <gda05189@gmail.com>
2023-11-02 20:24:08 +0900

    feat: MyFundingService findAllPostingByHost 추가

---
+++ src/main/java/com/theocean/fundering/domain/myfunding/service/MyFundingService.java
@@ -0,0 +1,20 @@
+package com.theocean.fundering.domain.myfunding.service;
+
+import com.theocean.fundering.domain.myfunding.dto.MyFundingResponseDTO;
+import com.theocean.fundering.domain.myfunding.repository.MyFundingRepository;
+import com.theocean.fundering.global.dto.PageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MyFundingService {
+    private final MyFundingRepository myFundingRepository;
+    public PageResponse<MyFundingResponseDTO> findAllPostingByHost(Long userId, Long postId, Pageable pageable) {
+        var page = myFundingRepository.findAllPostingByHost(userId, postId, pageable);
+        return new PageResponse<>(page);
+    }
+}
