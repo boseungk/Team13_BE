84fa3cf7138a5eaaf762d34fe59536803d49b3ab
BoSeung <gda05189@gmail.com>
2023-11-03 00:07:47 +0900

    feat: MyFundingService findAllPostingBySupporter 추가

--- src/main/java/com/theocean/fundering/domain/myfunding/service/MyFundingService.java
+++ src/main/java/com/theocean/fundering/domain/myfunding/service/MyFundingService.java
@@ -1,20 +1,24 @@
 package com.theocean.fundering.domain.myfunding.service;
 
-import com.theocean.fundering.domain.myfunding.dto.MyFundingResponseDTO;
+import com.theocean.fundering.domain.myfunding.dto.MyFundingHostResponseDTO;
+import com.theocean.fundering.domain.myfunding.dto.MyFundingSupporterResponseDTO;
 import com.theocean.fundering.domain.myfunding.repository.MyFundingRepository;
 import com.theocean.fundering.global.dto.PageResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class MyFundingService {
     private final MyFundingRepository myFundingRepository;
-    public PageResponse<MyFundingResponseDTO> findAllPostingByHost(Long userId, Long postId, Pageable pageable) {
+    public PageResponse<MyFundingHostResponseDTO> findAllPostingByHost(Long userId, Long postId, Pageable pageable) {
         var page = myFundingRepository.findAllPostingByHost(userId, postId, pageable);
+        return new PageResponse<>(page);
+    }
+
+    public PageResponse<MyFundingSupporterResponseDTO> findAllPostingBySupporter(Long userId, Long postId, Pageable pageable) {
+        var page = myFundingRepository.findAllPostingBySupporter(userId, postId, pageable);
         return new PageResponse<>(page);
     }
 }
