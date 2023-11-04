8590bbe9d011fe7c1253a82f7ae4e5943293e0f7
BoSeung <gda05189@gmail.com>
2023-11-04 22:44:38 +0900

    feat: MemberController 회원 정보 조회 및 수정 기능 추가

--- src/main/java/com/theocean/fundering/domain/member/controller/MemberController.java
+++ src/main/java/com/theocean/fundering/domain/member/controller/MemberController.java
@@ -1,17 +1,17 @@
 package com.theocean.fundering.domain.member.controller;
 
 import com.theocean.fundering.domain.member.dto.EmailRequestDTO;
+import com.theocean.fundering.domain.member.dto.MemberSettingRequestDTO;
 import com.theocean.fundering.domain.member.service.MemberService;
-import com.theocean.fundering.domain.member.dto.MemberRequestDTO;
+import com.theocean.fundering.domain.member.dto.MemberSignUpRequestDTO;
+import com.theocean.fundering.global.jwt.userInfo.CustomUserDetails;
 import com.theocean.fundering.global.utils.ApiUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,9 +25,28 @@ public class MemberController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody @Valid final MemberRequestDTO requestDTO, Error error){
+    public ResponseEntity<?> signUp(@RequestBody @Valid final MemberSignUpRequestDTO requestDTO, Error error){
         memberService.signUp(requestDTO);
         return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/user/setting")
+    public ResponseEntity<?> findAllUserSetting(
+            @AuthenticationPrincipal final CustomUserDetails userDetails
+    ){
+        var responseDTO = memberService.findAllUserSetting(userDetails.getId());
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PutMapping("/user/setting")
+    public ResponseEntity<?> modifyUserSetting(
+            @RequestBody @Valid final MemberSettingRequestDTO requestDTO, Error error,
+            @AuthenticationPrincipal final CustomUserDetails userDetails
+    ){
+        memberService.modifyUserSetting(requestDTO, userDetails.getId());
+        return ResponseEntity.ok().body(ApiUtils.success("성공적으로 변경되었습니다."));
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
