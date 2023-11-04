a28f880d7c3ee47786cbcd636da3bb4e3c547e25
BoSeung <gda05189@gmail.com>
2023-11-04 23:34:34 +0900

    feat: MyFundingService 팔로잉한 셀럽 조회 기능 추가

--- src/main/java/com/theocean/fundering/domain/myfunding/service/MyFundingService.java
+++ src/main/java/com/theocean/fundering/domain/myfunding/service/MyFundingService.java
@@ -1,7 +1,11 @@
 package com.theocean.fundering.domain.myfunding.service;
 
+import com.theocean.fundering.domain.celebrity.domain.Celebrity;
+import com.theocean.fundering.domain.celebrity.repository.CelebRepository;
+import com.theocean.fundering.domain.celebrity.repository.FollowRepository;
 import com.theocean.fundering.domain.member.domain.Member;
 import com.theocean.fundering.domain.member.repository.MemberRepository;
+import com.theocean.fundering.domain.myfunding.dto.MyFundingFollowingCelebsDTO;
 import com.theocean.fundering.domain.myfunding.dto.MyFundingHostResponseDTO;
 import com.theocean.fundering.domain.myfunding.dto.MyFundingSupporterResponseDTO;
 import com.theocean.fundering.domain.myfunding.repository.MyFundingRepository;
@@ -11,11 +15,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class MyFundingService {
     private final MyFundingRepository myFundingRepository;
     private final MemberRepository memberRepository;
+    private final FollowRepository followRepository;
+    private final CelebRepository celebRepository;
     public PageResponse<MyFundingHostResponseDTO> findAllPostingByHost(final Long userId, final Long postId, final Pageable pageable) {
         final var page = myFundingRepository.findAllPostingByHost(userId, postId, pageable);
         return new PageResponse<>(page);
@@ -31,5 +40,18 @@ public class MyFundingService {
                 () -> new Exception400("회원을 찾을 수 없습니다.")
         );
         return member.getNickname();
+    }
+
+    public List<MyFundingFollowingCelebsDTO> findFollowingCelebs(final Long userId) {
+        final List<MyFundingFollowingCelebsDTO> responseDTO = new ArrayList<>();
+        final List<Long> allFollowingCelebId = followRepository.findAllFollowingCelebById(userId);
+        for(final Long celebId: allFollowingCelebId){
+            final Celebrity celebrity = celebRepository.findById(celebId).orElseThrow(
+                    () -> new Exception400("셀럽을 찾을 수 없습니다.")
+            );
+            final int followerCount = followRepository.countByCelebId(celebId);
+            responseDTO.add(MyFundingFollowingCelebsDTO.of(celebrity, followerCount));
+        }
+        return responseDTO;
     }
 }
