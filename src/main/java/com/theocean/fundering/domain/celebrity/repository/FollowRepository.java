c99bdf93e8f1030c1c878ca33b0492f22fece916
BoSeung <gda05189@gmail.com>
2023-11-04 17:42:56 +0900

    feat: FollowRepository saveFollow 기능 추가

---
+++ src/main/java/com/theocean/fundering/domain/celebrity/repository/FollowRepository.java
@@ -0,0 +1,15 @@
+package com.theocean.fundering.domain.celebrity.repository;
+
+import com.theocean.fundering.domain.celebrity.domain.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface FollowRepository extends JpaRepository<Follow, Follow.PK> {
+    
+    @Modifying
+    @Query(value = "INSERT INTO follow(celebrity_id, member_id) VALUES(:celebrity_id, :member_id)", nativeQuery = true)
+    void saveFollow(@Param("celebrity_id") Long celebrity_id, @Param("member_id") Long member_id);
+
+}
