efc451b4411bcd5ba5f47579b7b3e8705bffddd3
BoSeung <gda05189@gmail.com>
2023-11-04 22:55:28 +0900

    feat: MyFundingService 회원 닉네임 조회 기능 추가

--- src/main/java/com/theocean/fundering/domain/myfunding/service/MyFundingService.java
+++ src/main/java/com/theocean/fundering/domain/myfunding/service/MyFundingService.java
@@ -1,9 +1,12 @@
 package com.theocean.fundering.domain.myfunding.service;
 
+import com.theocean.fundering.domain.member.domain.Member;
+import com.theocean.fundering.domain.member.repository.MemberRepository;
 import com.theocean.fundering.domain.myfunding.dto.MyFundingHostResponseDTO;
 import com.theocean.fundering.domain.myfunding.dto.MyFundingSupporterResponseDTO;
 import com.theocean.fundering.domain.myfunding.repository.MyFundingRepository;
 import com.theocean.fundering.global.dto.PageResponse;
+import com.theocean.fundering.global.errors.exception.Exception400;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,13 +15,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyFundingService {
     private final MyFundingRepository myFundingRepository;
-    public PageResponse<MyFundingHostResponseDTO> findAllPostingByHost(Long userId, Long postId, Pageable pageable) {
-        var page = myFundingRepository.findAllPostingByHost(userId, postId, pageable);
+    private final MemberRepository memberRepository;
+    public PageResponse<MyFundingHostResponseDTO> findAllPostingByHost(final Long userId, final Long postId, final Pageable pageable) {
+        final var page = myFundingRepository.findAllPostingByHost(userId, postId, pageable);
         return new PageResponse<>(page);
     }
 
-    public PageResponse<MyFundingSupporterResponseDTO> findAllPostingBySupporter(Long userId, Long postId, Pageable pageable) {
-        var page = myFundingRepository.findAllPostingBySupporter(userId, postId, pageable);
+    public PageResponse<MyFundingSupporterResponseDTO> findAllPostingBySupporter(final Long userId, final Long postId, final Pageable pageable) {
+        final var page = myFundingRepository.findAllPostingBySupporter(userId, postId, pageable);
         return new PageResponse<>(page);
+    }
+
+    public String getNickname(final Long id) {
+        final Member member = memberRepository.findById(id).orElseThrow(
+                () -> new Exception400("회원을 찾을 수 없습니다.")
+        );
+        return member.getNickname();
     }
 }
