16c9528b81a08c1ff646a257ee5f93ddb0b6df69
BoSeung <gda05189@gmail.com>
2023-11-05 20:02:36 +0900

    style: Follow 변수 명 변경

--- src/main/java/com/theocean/fundering/domain/celebrity/domain/Follow.java
+++ src/main/java/com/theocean/fundering/domain/celebrity/domain/Follow.java
@@ -11,17 +11,13 @@ import java.io.Serializable;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(
         name = "follow",
-        indexes = {
-                @Index(name = "idx_celebrity_id", columnList = "celebrity_id"),
-                @Index(name = "idx_member_id", columnList = "member_id")
-        },
         uniqueConstraints = @UniqueConstraint(columnNames = {"celebrity_id", "member_id"}))
 @IdClass(Follow.PK.class)
 @Entity
 public class Follow{
     @Id
     @Column(name="celebrity_id", insertable = false, updatable = false)
-    private Long celebrityId;
+    private Long celebId;
 
     @Id
     @Column(name="member_id", insertable = false, updatable = false)
@@ -29,7 +25,7 @@ public class Follow{
 
     //Follow 관계의 유일성을 위한 복합키 설정
     public static class PK implements Serializable {
-        Long celebrityId;
+        Long celebId;
         Long memberId;
     }
 }
