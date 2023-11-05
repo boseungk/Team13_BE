2a2f7b20632a43124c8a6ee3daab795ca485e5bc
BoSeung <gda05189@gmail.com>
2023-11-05 20:02:14 +0900

    style: FollowRepository 변수명 변경

--- src/main/java/com/theocean/fundering/domain/celebrity/repository/FollowRepository.java
+++ src/main/java/com/theocean/fundering/domain/celebrity/repository/FollowRepository.java
@@ -9,20 +9,20 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Follow.PK> {
-    @Query("SELECT COUNT(*) FROM Follow f WHERE f.celebrityId = :celebId")
+    @Query("SELECT COUNT(*) FROM Follow f WHERE f.celebId = :celebId")
     int countByCelebId(@Param("celebId") Long celebId);
 
-    @Query("SELECT COUNT(*) FROM Follow f WHERE f.celebrityId = :celebId AND f.memberId = :followId")
+    @Query("SELECT COUNT(*) FROM Follow f WHERE f.celebId = :celebId AND f.memberId = :followId")
     int countByCelebIdAndFollowId(@Param("celebId") Long celebId, @Param("followId") Long followId);
 
-    @Query("SELECT f.celebrityId FROM Follow f WHERE f.memberId = :memberId ")
+    @Query("SELECT f.celebId FROM Follow f WHERE f.memberId = :memberId ")
     List<Long> findAllFollowingCelebById(@Param("memberId") Long memberId);
 
     @Modifying
-    @Query(value = "INSERT INTO follow(celebrity_id, member_id) VALUES(:celebrity_id, :member_id)", nativeQuery = true)
-    void saveFollow(@Param("celebrity_id") Long celebrity_id, @Param("member_id") Long member_id);
+    @Query(value = "INSERT INTO follow(celebrity_id, member_id) VALUES(:celebId, :memberId)", nativeQuery = true)
+    void saveFollow(@Param("celebId") Long celebId, @Param("memberId") Long memberId);
 
     @Modifying
-    @Query(value = "DELETE FROM follow WHERE celebrity_id = :celebrity_id AND member_id = :member_id", nativeQuery = true)
-    void saveUnFollow(@Param("celebrity_id") Long celebrity_id, @Param("member_id") Long member_id);
+    @Query(value = "DELETE FROM follow WHERE celebrity_id = :celebId AND member_id = :memberId", nativeQuery = true)
+    void saveUnFollow(@Param("celebId") Long celebId, @Param("memberId") Long memberId);
 }
