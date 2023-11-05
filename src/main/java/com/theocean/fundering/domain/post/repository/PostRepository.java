1799d525805f4e9229551861929b82967337c440
BoSeung <gda05189@gmail.com>
2023-11-05 20:00:28 +0900

    feat: PostRepository findPostbyCelebId 기능 추가

--- src/main/java/com/theocean/fundering/domain/post/repository/PostRepository.java
+++ src/main/java/com/theocean/fundering/domain/post/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.theocean.fundering.domain.post.repository;
 
 import com.theocean.fundering.domain.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -10,4 +12,6 @@ import java.util.Optional;
 
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostQuerydslRepository {
+    @Query("SELECT p FROM Post p WHERE p.celebrity.celebId = :celebId")
+    List<Post> findPostByCelebId(@Param("celebId") Long celebId);
 }
