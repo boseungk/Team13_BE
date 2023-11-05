4a471809df47b5c7fb47ef4e31f0249ad0002693
BoSeung <gda05189@gmail.com>
2023-11-05 19:55:45 +0900

    refactor: Celebrity column 이름 변경 및 FollowerCount 기능 추가

--- src/main/java/com/theocean/fundering/domain/celebrity/domain/Celebrity.java
+++ src/main/java/com/theocean/fundering/domain/celebrity/domain/Celebrity.java
@@ -1,8 +1,8 @@
 package com.theocean.fundering.domain.celebrity.domain;
 
 import com.theocean.fundering.domain.celebrity.domain.constant.ApprovalStatus;
+import com.theocean.fundering.domain.celebrity.domain.constant.CelebCategory;
 import com.theocean.fundering.domain.celebrity.domain.constant.CelebGender;
-import com.theocean.fundering.domain.celebrity.domain.constant.CelebType;
 import com.theocean.fundering.global.utils.AuditingFields;
 import jakarta.persistence.*;
 import lombok.*;
@@ -13,34 +13,39 @@ import java.util.Objects;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "celebrity",
-        indexes = @Index(columnList = "celebName")
+        indexes = {
+            @Index(columnList = "name"), @Index(columnList = "follower_count")
+        }
 )
 @Entity
 public class Celebrity extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "celebrity_id")
     private Long celebId;
 
-    @Column(nullable = false, length = 15)
+    @Column(nullable = false, length = 15, name = "name")
     private String celebName;
 
+    @Column(name = "follower_count")
+    private int followerCount;
+
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, name = "gender")
     private CelebGender celebGender;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private CelebType celebType;
+    @Column(nullable = false, name = "celeb_type")
+    private CelebCategory celebCategory;
 
-    @Column(length = 50)
+    @Column(length = 50, name = "celeb_group")
     private String celebGroup;
 
-    @Column(nullable = false)
+    @Column(name = "profile_image")
     private String profileImage;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, name = "approval_status")
     private ApprovalStatus status = ApprovalStatus.PENDING;
 
     public Celebrity approvalCelebrity(){
@@ -48,12 +53,24 @@ public class Celebrity extends AuditingFields {
         return this;
     }
 
+    public void addFollowerCount() {
+        followerCount += 1;
+    }
+
+    public void minusFollowerCount(){
+        followerCount -= 1;
+    }
+
+    public void updateProfileImage(String thumbnail){
+        profileImage = thumbnail;
+    }
+
     @Builder
-    public Celebrity(String celebName, CelebGender celebGender, CelebType celebType,
+    public Celebrity(String celebName, CelebGender celebGender, CelebCategory celebCategory,
                      String celebGroup, String profileImage) {
         this.celebName = celebName;
         this.celebGender = celebGender;
-        this.celebType = celebType;
+        this.celebCategory = celebCategory;
         this.celebGroup = celebGroup;
         this.profileImage = profileImage;
     }
@@ -69,4 +86,6 @@ public class Celebrity extends AuditingFields {
     public int hashCode() {
         return Objects.hash(celebId);
     }
+
+
 }
