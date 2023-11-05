f60c490ed30f2f352843290a45209a4e81907435
46112 <duf325@naver.com>
2023-11-05 16:28:14 +0900

    refactor: 하드코딩된 memberId 부분 리팩토링

--- src/main/java/com/theocean/fundering/domain/comment/controller/CommentController.java
+++ src/main/java/com/theocean/fundering/domain/comment/controller/CommentController.java
@@ -7,14 +7,22 @@ import com.theocean.fundering.domain.comment.service.DeleteCommentService;
 import com.theocean.fundering.domain.comment.service.ReadCommentService;
 import com.theocean.fundering.global.jwt.userInfo.CustomUserDetails;
 import com.theocean.fundering.global.utils.ApiUtils;
+
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,11 +36,11 @@ public class CommentController {
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<?> createComment(
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody @Valid CommentRequest.SaveDTO commentRequest,
-            @PathVariable long postId) {
+            @AuthenticationPrincipal final CustomUserDetails userDetails,
+            @RequestBody @Valid final CommentRequest.SaveDTO commentRequest,
+            @PathVariable final long postId) {
 
-        Long memberId = 1L; // Long memberId = userDetails.getMember().getUserId();
+        final Long memberId = userDetails.getId();
         createCommentService.createComment(memberId, postId, commentRequest);
 
         return ResponseEntity.ok(ApiUtils.success(null));
@@ -42,12 +50,12 @@ public class CommentController {
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<?> createSubComment(
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody @Valid CommentRequest.SaveDTO commentRequest,
-            @PathVariable long postId,
-            @PathVariable long commentId) {
+            @AuthenticationPrincipal final CustomUserDetails userDetails,
+            @RequestBody @Valid final CommentRequest.SaveDTO commentRequest,
+            @PathVariable final long postId,
+            @PathVariable final long commentId) {
 
-        Long memberId = 1L; // Long memberId = userDetails.getMember().getUserId();
+        final Long memberId = userDetails.getId();
         createCommentService.createSubComment(memberId, postId, commentId, commentRequest);
 
         return ResponseEntity.ok(ApiUtils.success(null));
@@ -56,9 +64,9 @@ public class CommentController {
     // (기능) 댓글 목록 조회
     @GetMapping("/posts/{postId}/comments")
     public ResponseEntity<?> readComments(
-            @PathVariable long postId, @PageableDefault(size = 10) Pageable pageable) {
+            @PathVariable final long postId, @PageableDefault(size = 10) final Pageable pageable) {
 
-        CommentResponse.FindAllDTO response = readCommentService.getComments(postId, pageable);
+        final CommentResponse.FindAllDTO response = readCommentService.getComments(postId, pageable);
 
         return ResponseEntity.ok(ApiUtils.success(response));
     }
@@ -66,11 +74,11 @@ public class CommentController {
     // (기능) 대댓글 목록 조회
     @GetMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<?> readSubComments(
-            @PathVariable long postId,
-            @PathVariable long commentId,
-            @PageableDefault(size = 10) Pageable pageable) {
+            @PathVariable final long postId,
+            @PathVariable final long commentId,
+            @PageableDefault(size = 10) final Pageable pageable) {
 
-        CommentResponse.FindAllDTO response =
+        final CommentResponse.FindAllDTO response =
                 readCommentService.getSubComments(postId, commentId, pageable);
 
         return ResponseEntity.ok(ApiUtils.success(response));
@@ -80,11 +88,11 @@ public class CommentController {
     @PreAuthorize("hasRole('USER')")
     @DeleteMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<?> deleteComment(
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-            @PathVariable long postId,
-            @PathVariable long commentId) {
+            @AuthenticationPrincipal final CustomUserDetails userDetails,
+            @PathVariable final long postId,
+            @PathVariable final long commentId) {
 
-        Long memberId = 1L; // userDetails.getMember().getUserId();
+        final Long memberId = userDetails.getId();
         deleteCommentService.deleteComment(memberId, postId, commentId);
 
         return ResponseEntity.ok(ApiUtils.success(null));
