c441adb20f8cf2476e0838a1e9c23fb58d695143
46112 <duf325@naver.com>
2023-11-07 00:59:38 +0900

    refactor: PostRequest 필드값 EpochSecond로 변경
    - toEntity를 만족시키기 위해 toLocalDateTime메서드 추가

--- src/main/java/com/theocean/fundering/domain/post/dto/PostRequest.java
+++ src/main/java/com/theocean/fundering/domain/post/dto/PostRequest.java
@@ -1,14 +1,27 @@
 package com.theocean.fundering.domain.post.dto;
 
-
 import com.theocean.fundering.domain.celebrity.domain.Celebrity;
 import com.theocean.fundering.domain.member.domain.Member;
 import com.theocean.fundering.domain.post.domain.Post;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class PostRequest {
+    private static long toEpochSecond(final LocalDateTime localDateTime) {
+        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+    }
+
+    private static LocalDateTime toLocalDateTime(final long epochSecond) {
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(epochSecond), ZoneId.systemDefault());
+    }
+
     @Getter
     @Setter
     @NoArgsConstructor
@@ -19,10 +32,10 @@ public class PostRequest {
         private String introduction;
         private String thumbnailURL;
         private int targetPrice;
-        private LocalDateTime deadline;
-        private LocalDateTime createdAt;
+        private Long deadline;
+        private Long createdAt;
 
-        public Post toEntity(Member writer, Celebrity celebrity){
+        public Post toEntity(Member writer, Celebrity celebrity) {
             return Post.builder()
                     .writer(writer)
                     .celebrity(celebrity)
@@ -30,53 +43,52 @@ public class PostRequest {
                     .introduction(introduction)
                     .thumbnail(thumbnailURL)
                     .targetPrice(targetPrice)
-                    .deadline(deadline)
+                    .deadline(toLocalDateTime(deadline))
                     .build();
         }
 
         @Builder
-        public PostWriteDTO(Long celebId, String title, String introduction, String thumbnail, int targetPrice, LocalDateTime deadline){
+        public PostWriteDTO(Long celebId, String title, String introduction, String thumbnail, int targetPrice, Long deadline) {
             this.celebId = celebId;
             this.title = title;
             this.introduction = introduction;
             this.thumbnailURL = thumbnail;
             this.targetPrice = targetPrice;
             this.deadline = deadline;
-            this.createdAt = LocalDateTime.now();
+            this.createdAt = toEpochSecond(LocalDateTime.now());
         }
-
     }
 
     @Getter
     @Setter
     @NoArgsConstructor
     @ToString
-    public static class PostEditDTO{
+    public static class PostEditDTO {
         private String title;
         private String introduction;
         private String thumbnail;
         private int targetPrice;
-        private LocalDateTime deadline;
-        private LocalDateTime modifiedAt;
+        private Long deadline;
+        private Long modifiedAt;
 
-        public Post toEntity(){
+        public Post toEntity() {
             return Post.builder()
                     .title(title)
                     .introduction(introduction)
                     .thumbnail(thumbnail)
                     .targetPrice(targetPrice)
-                    .deadline(deadline)
+                    .deadline(toLocalDateTime(deadline))
                     .build();
         }
+
         @Builder
-        public PostEditDTO(String title, String introduction, String thumbnail, int targetPrice, LocalDateTime deadline){
+        public PostEditDTO(String title, String introduction, String thumbnail, int targetPrice, Long deadline) {
             this.title = title;
             this.introduction = introduction;
             this.thumbnail = thumbnail;
             this.targetPrice = targetPrice;
             this.deadline = deadline;
-            this.modifiedAt = LocalDateTime.now();
+            this.modifiedAt = toEpochSecond(LocalDateTime.now());
         }
     }
-
 }
