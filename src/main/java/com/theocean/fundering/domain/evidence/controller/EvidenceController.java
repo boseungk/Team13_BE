ece7c8fd4d74b4da1118e6039b2eb8054eafae64
46112 <duf325@naver.com>
2023-11-08 21:07:54 +0900

    fix: 누락된 import문 생성

--- src/main/java/com/theocean/fundering/domain/evidence/controller/EvidenceController.java
+++ src/main/java/com/theocean/fundering/domain/evidence/controller/EvidenceController.java
@@ -15,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
