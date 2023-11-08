7cf03e9e784a00434949545d47a4b240fa27c782
46112 <duf325@naver.com>
2023-11-08 20:54:31 +0900

    refactor: evidenceController 와일드카드 제거

--- src/main/java/com/theocean/fundering/domain/evidence/controller/EvidenceController.java
+++ src/main/java/com/theocean/fundering/domain/evidence/controller/EvidenceController.java
@@ -13,7 +13,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "EVIDENCE", description = "증빙 관련 API")
